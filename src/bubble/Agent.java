6fd5b4e3d15c56a9255b3e9e6b423341faa54b21
eXcellenceSPb <31094789+eXcellenceSPb@users.noreply.github.com>
2018-02-15 17:35:44 +0300

    Update Agent.java

--- src/bubble/Agent.java
+++ src/bubble/Agent.java
@@ -1,11 +1,11 @@
 package bubble;
 
-public class Agent {
-	public static void premain(String args) {
-		new Thread(new Runnable() {
-			public void run() {
-				new Enemy(10, 1);
-			}
-		});
-	}
-}
+// public class Agent {
+// 	public static void premain(String args) {
+// 		new Thread(new Runnable() {
+// 			public void run() {
+// 				new Enemy(10, 1);
+// 			}
+// 		});
+// 	}
+// }
